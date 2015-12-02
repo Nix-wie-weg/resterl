06569ed0aa402f01a140d5ea60adb6ac7f5dc496
Florian Dütsch <florian.duetsch@nix-wie-weg.de>
2015-12-02 09:14:36 +0100

    #19742: RuboCop for base_object.rb

--- lib/resterl/base_object.rb
+++ lib/resterl/base_object.rb
@@ -1,90 +1,83 @@
 require 'hashie'
 
-class Resterl::BaseObject #< Hashie::Mash
+module Resterl
+  class BaseObject
+    include ClassLevelInheritableAttributes
+    cattr_inheritable :resterl_client, :parser, :composer, :complete_mime_type,
+                      :mapper
+    attr_reader :response
 
-  include ClassLevelInheritableAttributes
-  cattr_inheritable :resterl_client, :parser, :composer, :complete_mime_type,
-                    :mapper
-  attr_reader :response
+    def initialize data = {}, response = nil
+      @data = Hashie::Mash.new data
+      @response = response
+    end
 
-  #self.resterl_client = nil
-  #self.complete_mime_type = 'text/plain'
+    def method_missing sym, *args, &block
+      @data.send sym, *args, &block
+    end
 
-  def initialize data = {}, response = nil
-    @data = Hashie::Mash.new data
-    @response = response
-  end
+    def respond_to? sym
+      @data.respond_to? sym
+    end
 
-  def method_missing sym, *args, &block
-    @data.send sym, *args, &block
-  end
+    def self.get_object url, params = {}
+      headers = { 'Accept' => complete_mime_type }
+      response = resterl_client.get(url, params, headers)
+      doc = response.body
+      doc = parser.call(doc)
+      doc = mapper.map(doc) if @mapper
+      new(doc, response)
+    end
 
-  def respond_to? sym
-    @data.respond_to? sym
-  end
+    def self.post_to_object url, params = {}, data = {}
+      headers = {
+        'Accept' => complete_mime_type,
+        'Content-Type' => complete_mime_type
+      }
+      data = composer.call(data)
+      response = resterl_client.post(url, params, data, headers)
 
-  protected
+      doc = response.body
+      doc = parser.call(doc)
+      doc = mapper.map(doc) if @mapper
+      new(doc, response)
+    end
 
-  def self.get_object url, params = {}
-    headers = { 'Accept' => complete_mime_type }
-    response = resterl_client.get(url, params, headers)
-    doc = response.body
-    doc = parser.call(doc)
-    doc = mapper.map(doc) if @mapper
-    new(doc, response)
-  end
+    def self.delete_object url
+      headers = { 'Accept' => complete_mime_type }
+      resterl_client.delete(url, {}, headers, {})
+    end
 
-  def self.post_to_object url, params = {}, data = {}
-    headers = {
-      'Accept' => complete_mime_type,
-      'Content-Type' => complete_mime_type
-    }
-    data = composer.call(data)
-    response = resterl_client.post(url, params, data, headers)
+    def self.put_object url, params = {}, data
+      # TODO: Refactoring
 
-    doc = response.body
-    doc = parser.call(doc)
-    doc = mapper.map(doc) if @mapper
-    new(doc, response)
-  end
+      headers = {
+        'Accept' => complete_mime_type,
+        'Content-Type' => complete_mime_type
+      }
+      data = composer.call(data)
+      response = resterl_client.put(url, params, data, headers)
 
-  def self.delete_object url
-    headers = {
-      'Accept' => complete_mime_type,
-    }
-    resterl_client.delete(url, {}, headers, {})
-  end
+      doc = response.body
+      doc = parser.call(doc)
+      doc = mapper.map(doc) if @mapper
+      new(doc, response)
+    end
 
-  def self.put_object url, params = {}, data
-    # TODO: Refactoring
-
-    headers = {
-      'Accept' => complete_mime_type,
-      'Content-Type' => complete_mime_type
-    }
-    data = composer.call(data)
-    response = resterl_client.put(url, params, data, headers)
-
-    doc = response.body
-    doc = parser.call(doc)
-    doc = mapper.map(doc) if @mapper
-    new(doc, response)
-  end
-
-
-  def self.mime_type= t
-    self.parser, self.composer, self.complete_mime_type = case t
-    when :json
-      [ proc {|str| JSON.parse(str)},
-        proc(&:to_json),
-        'application/json'
-      ]
-    when :xml
-      [ proc {|str| Hash.from_xml(str)},
-        proc(&:to_xml),
-        'application/xml'
-      ]
+    def self.mime_type= t
+      self.parser, self.composer, self.complete_mime_type =
+        case t
+        when :json
+          [proc { |str| JSON.parse(str) },
+           proc(&:to_json),
+           'application/json'
+          ]
+        when :xml
+          [proc { |str| Hash.from_xml(str) },
+           proc(&:to_xml),
+           'application/xml'
+          ]
+        end
     end
   end
-
 end
