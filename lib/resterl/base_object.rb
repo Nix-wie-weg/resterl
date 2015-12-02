02b8306d4c4185324c537b63b18828b3211252a9
Florian Dütsch <florian.duetsch@nix-wie-weg.de>
2015-12-02 09:23:07 +0100

    #19742: Refactorings for Resterl::BaseObject

--- lib/resterl/base_object.rb
+++ lib/resterl/base_object.rb
@@ -20,64 +20,61 @@ module Resterl
       @data.respond_to? sym
     end
 
-    def self.get_object url, params = {}
-      headers = { 'Accept' => complete_mime_type }
-      response = resterl_client.get(url, params, headers)
-      doc = response.body
-      doc = parser.call(doc)
-      doc = mapper.map(doc) if @mapper
-      new(doc, response)
-    end
+    class << self
+      def get_object url, params = {}
+        response = resterl_client.get(url, params, accept_header)
+        new_from_response(response)
+      end
 
-    def self.post_to_object url, params = {}, data = {}
-      headers = {
-        'Accept' => complete_mime_type,
-        'Content-Type' => complete_mime_type
-      }
-      data = composer.call(data)
-      response = resterl_client.post(url, params, data, headers)
+      def post_to_object url, params = {}, data = {}
+        post_put_object(:post, url, params, data)
+      end
 
-      doc = response.body
-      doc = parser.call(doc)
-      doc = mapper.map(doc) if @mapper
-      new(doc, response)
-    end
+      def delete_object url
+        resterl_client.delete(url, {}, accept_header, {})
+      end
 
-    def self.delete_object url
-      headers = { 'Accept' => complete_mime_type }
-      resterl_client.delete(url, {}, headers, {})
-    end
+      def put_object url, params = {}, data = {}
+        post_put_object(:put, url, params, data)
+      end
 
-    def self.put_object url, params = {}, data
-      # TODO: Refactoring
+      def mime_type= t
+        self.parser, self.composer, self.complete_mime_type =
+          case t
+          when :json
+            [proc { |str| JSON.parse(str) },
+             proc(&:to_json),
+             'application/json'
+            ]
+          when :xml
+            [proc { |str| Hash.from_xml(str) },
+             proc(&:to_xml),
+             'application/xml'
+            ]
+          end
+      end
 
-      headers = {
-        'Accept' => complete_mime_type,
-        'Content-Type' => complete_mime_type
-      }
-      data = composer.call(data)
-      response = resterl_client.put(url, params, data, headers)
+      private
 
-      doc = response.body
-      doc = parser.call(doc)
-      doc = mapper.map(doc) if @mapper
-      new(doc, response)
-    end
+      def post_put_object method, url, params, data
+        headers = accept_header.merge(
+          'Content-Type' => complete_mime_type
+        )
+        data = composer.call(data)
+        response = resterl_client.send(method, url, params, data, headers)
+        new_from_response response
+      end
 
-    def self.mime_type= t
-      self.parser, self.composer, self.complete_mime_type =
-        case t
-        when :json
-          [proc { |str| JSON.parse(str) },
-           proc(&:to_json),
-           'application/json'
-          ]
-        when :xml
-          [proc { |str| Hash.from_xml(str) },
-           proc(&:to_xml),
-           'application/xml'
-          ]
-        end
+      def new_from_response response
+        doc = response.body
+        doc = parser.call(doc)
+        doc = mapper.map(doc) if @mapper
+        new(doc, response)
+      end
+
+      def accept_header
+        { 'Accept' => complete_mime_type }
+      end
     end
   end
 end
